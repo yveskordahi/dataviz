309a262eea7acd480ccbbcc0bb7084692f4681cb
Yves Kordahi <y.korahi@outlook.fr>
2022-02-16 22:00:30 +0100

    dataviz

---
+++ script.js
@@ -0,0 +1,26 @@
+// Create map instance
+var chart = am4core.create("chartdiv", am4maps.MapChart);
+
+// Set map definition
+chart.geodata = am4geodata_worldLow;
+
+// Set projection
+chart.projection = new am4maps.projections.Miller();
+
+// Create map polygon series
+var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+
+// Make map load polygon (like country names) data from GeoJSON
+polygonSeries.useGeodata = true;
+
+// Configure series
+var polygonTemplate = polygonSeries.mapPolygons.template;
+polygonTemplate.tooltipText = "{name}";
+polygonTemplate.fill = am4core.color("#74B266");
+
+// Create hover state and set alternative fill color
+var hs = polygonTemplate.states.create("hover");
+hs.properties.fill = am4core.color("#367B25");
+
+// Remove Antarctica
+polygonSeries.include = ["PT", "ES", "FR", "DE", "BE", "NL", "IT", "AT", "IE", "CH", "LU", "LT", "LV", "CZ", "SK", "SI", "EE", "HU", "CY", "MT", "PL", "RO", "BG", "HR", "DK", "FI", "SE"];
