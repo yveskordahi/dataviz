8a95827b12cae4038eb6d0d720d6aa95c62ed4e9
Yves Kordahi <y.korahi@outlook.fr>
2022-02-16 23:14:33 +0100

    dataviz

--- script.js
+++ script.js
@@ -1,148 +1,47 @@
-// // Create map instance
-// var chart = am4core.create("chartdiv", am4maps.MapChart);
-
-// // Set map definition
-// chart.geodata = am4geodata_worldLow;
-
-// // Set projection
-// chart.projection = new am4maps.projections.Miller();
-
-// // Create map polygon series
-// var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-// // Make map load polygon (like country names) data from GeoJSON
-// polygonSeries.useGeodata = true;
-
-// // Configure series
-// var polygonTemplate = polygonSeries.mapPolygons.template;
-// polygonTemplate.tooltipText = "{name}";
-// polygonTemplate.fill = am4core.color("#74B266");
-
-// // Create hover state and set alternative fill color
-// var hs = polygonTemplate.states.create("hover");
-// hs.properties.fill = am4core.color("#367B25");
-
-// // Remove Antarctica
-// polygonSeries.include = ["PT", "ES", "FR", "DE", "BE", "NL", "IT", "AT", "IE", "CH", "LU", "LT", "LV", "CZ", "SK", "SI", "EE", "HU", "CY", "MT", "PL", "RO", "BG", "HR", "DK", "FI", "SE"];
-
-/**
- * ---------------------------------------
- * This demo was created using amCharts 4.
- *
- * For more information visit:
- * https://www.amcharts.com/
- *
- * Documentation is available at:
- * https://www.amcharts.com/docs/v4/
- * ---------------------------------------
- */
-
-// /* Create map instance */
-// var chart = am4core.create("chartdiv", am4maps.MapChart);
-
-// /* Set map definition */
-// chart.geodata = am4geodata_worldLow;
-
-// /* Set projection */
-// chart.projection = new am4maps.projections.Mercator();
-
-// /* Northern Europe */
-// var series1 = chart.series.push(new am4maps.MapPolygonSeries());
-// series1.name = "Northern Europe";
-// series1.useGeodata = true;
-// series1.include = ["FI", "DK", "SE", "LT", "LV", "EE",];
-// series1.mapPolygons.template.tooltipText = "{name}";
-// series1.mapPolygons.template.fill = am4core.color("#96BDC6");
-// series1.fill = am4core.color("#96BDC6");
-
-// /* Central Europe */
-// var series2 = chart.series.push(new am4maps.MapPolygonSeries());
-// series2.name = "Central Europe";
-// series2.useGeodata = true;
-// series2.include = ["AT", "CZ", "DE", "HU", "PL", "SK", "SI"];
-// series2.mapPolygons.template.tooltipText = "{name}";
-// series2.mapPolygons.template.fill = am4core.color("#81968F");
-// series2.fill = am4core.color("#81968F");
-
-// /* Southeast Europe */
-// var series4 = chart.series.push(new am4maps.MapPolygonSeries());
-// series4.name = "Southeast Europe";
-// series4.useGeodata = true;
-// series4.include = ["BG", "HR", "RO", "GR"];
-// series4.mapPolygons.template.tooltipText = "{name}";
-// series4.mapPolygons.template.fill = am4core.color("#E8CCBF");
-// series4.fill = am4core.color("#E8CCBF");
-
-// /* Western Europe */
-// var series5 = chart.series.push(new am4maps.MapPolygonSeries());
-// series5.name = "Western Europe";
-// series5.useGeodata = true;
-// series5.include = ["BE", "FR", "IT", "LU", "NL", "ES", "PT"];
-// series5.mapPolygons.template.tooltipText = "{name}";
-// series5.mapPolygons.template.fill = am4core.color("#99C78F");
-// series5.fill = am4core.color("#99C78F");
-
-// chart.legend = new am4maps.Legend();
-// chart.legend.position = "right";
-// chart.legend.align = "right";
-
-/**
- * ---------------------------------------
- * This demo was created using amCharts 5.
- * 
- * For more information visit:
- * https://www.amcharts.com/
- * 
- * Documentation is available at:
- * https://www.amcharts.com/docs/v5/
- * ---------------------------------------
- */
-
 // Data
 var groupData = [
     {
-      "name": "EU member before 2004",
+      "name": "Plus de 2% du PIB",
       "data": [
-        { "id": "AT", "joined": "1995"},
-        { "id": "IE", "joined": "1973"},
-        { "id": "DK", "joined": "1973"},
-        { "id": "FI", "joined": "1995"},
-        { "id": "SE", "joined": "1995"},
-        { "id": "GB", "joined": "1973"},
-        { "id": "IT", "joined": "1957"},
-        { "id": "FR", "joined": "1957"},
-        { "id": "ES", "joined": "1986"},
-        { "id": "GR", "joined": "1981"},
-        { "id": "DE", "joined": "1957"},
-        { "id": "BE", "joined": "1957"},
-        { "id": "LU", "joined": "1957"},
-        { "id": "NL", "joined": "1957"},
-        { "id": "PT", "joined": "1986"}
+        { "id": "FR", "joined": "PIB: 2.1% [/]\n Budget: 52.747 milliards $"}, 
+        { "id": "GR", "joined": "PIB: 2.8% [/]\n Budget: 5.301 milliards $"},
+        { "id": "EE", "joined": "PIB: 2.3% [/]\n Budget: 0.701 milliards $"},
+        { "id": "LT", "joined": "PIB: 2.1% [/]\n Budget: 1.170 milliards $"},
+        { "id": "LV", "joined": "PIB: 2.3% [/]\n Budget: 0.756 milliards $"},
+        { "id": "RO", "joined": "PIB: 2.3% [/]\n Budget: 5.726 milliards $"},
+        { "id": "PL", "joined": "PIB: 2.2% [/]\n Budget: 13.026 milliards $"},
+        { "id": "PT", "joined": "PIB: 2% [/]\n Budget: 4.639 milliards $"}
      ]
     }, {
-      "name": "Joined at 2004",
+      "name": "Entre 1.5 et 2% du PIB",
       "data": [
-        { "id": "LT", "joined": "2004" },
-        { "id": "LV", "joined": "2004" },
-        { "id": "CZ", "joined": "2004" },
-        { "id": "SK", "joined": "2004" },
-        { "id": "SI", "joined": "2004" },
-        { "id": "EE", "joined": "2004" },
-        { "id": "HU", "joined": "2004" },
-        { "id": "CY", "joined": "2004" },
-        { "id": "MT", "joined": "2004" },
-        { "id": "PL", "joined": "2004" }
+        { "id": "IT", "joined": "PIB: 1.6% [/]\n Budget: 28.921 milliards $"},
+        { "id": "BG", "joined": "PIB: 1.8% [/]\n Budget: 1.247 milliards $"},
+        { "id": "HU", "joined": "PIB: 1.6% [/]\n Budget: 2.409 milliards $"},
+        { "id": "SK", "joined": "PIB: 1.8% [/]\n Budget: 1.837 milliards $"},
+        { "id": "FI", "joined": "PIB: 1.5% [/]\n Budget: 4.087 milliards $"},
+        { "id": "CY", "joined": "PIB: 1.8% [/]\n Budget: 0.418 milliards $"},
+        { "id": "HR", "joined": "PIB: 1.8% [/]\n Budget: 1.034 milliards $"}
       ]
     }, {
-      "name": "Joined at 2007",
+      "name": "Entre 1 et 1.5% du PIB",
       "data": [
-        { "id": "RO", "joined": "2007" },
-        { "id": "BG", "joined": "2007" }
+        { "id": "CZ", "joined": "PIB: 1.4% [/]\n Budget: 3.252 milliards $"},
+        { "id": "SI", "joined": "PIB: 1.1% [/]\n Budget: 0.574 milliards $"},
+        { "id": "ES", "joined": "PIB: 1.4% [/]\n Budget: 17.431 milliards $"},
+        { "id": "BE", "joined": "PIB: 1.1% [/]\n Budget: 5.461 milliards $"},
+        { "id": "NL", "joined": "PIB: 1.8% [/]\n Budget: 1.247 milliards $"},
+        { "id": "DE", "joined": "PIB: 1.4% [/]\n Budget: 12.578 milliards $"},
+        { "id": "SE", "joined": "PIB: 1.2% [/]\n Budget: 6.453 milliards $"},
+        { "id": "DK", "joined": "PIB: 1.4% [/]\n Budget: 4.953 milliards $"},
       ]
     }, {
-      "name": "Joined at 2013",
+      "name": "Moins de 1% du PIB",
       "data": [
-        { "id": "HR", "joined": "2013" }
+        { "id": "LU", "joined": "PIB: 0.8% [/]\n Budget: 0.489 milliards $"},
+        { "id": "IE", "joined": "PIB: 0.3% [/]\n Budget: 1.144 milliards $"},
+        { "id": "MT", "joined": "PIB: 0.6% [/]\n Budget: 0.080 milliards $"},
+        { "id": "AT", "joined": "PIB: 0.8% [/]\n Budget: 3.601 milliards $"}
       ]
     }
   ];
@@ -220,7 +119,7 @@ var groupData = [
   
   
     polygonSeries.mapPolygons.template.setAll({
-      tooltipText: "[bold]{name}[/]\nMember since {joined}",
+      tooltipText: "[bold]{name}[/]\n {joined}",
       interactive: true,
       fill: color,
       strokeWidth: 2
